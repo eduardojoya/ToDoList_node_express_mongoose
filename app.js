2c01eb9621e0012a3fe2b2b577e10e7527475268
eduardojoya <ebjoya@gmail.com>
2019-07-16 16:47:24 -0700

    reinstalled ejs

--- app.js
+++ app.js
@@ -154,4 +154,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
+});
