3a962d65d873eeee692620545c00f80ad896d1f3
eduardojoya <ebjoya@gmail.com>
2019-07-16 16:42:47 -0700

    modified port

--- app.js
+++ app.js
@@ -154,11 +154,4 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
-app.listen(port, function () {
-  console.log("Server has started successfully");
-});
+app.listen(process.env.PORT);
